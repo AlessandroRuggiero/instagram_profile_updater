71eba60d7360eb23e86ffcf880966d5d5c5cce97
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2024-05-24 12:00:55 +0200

    removed comments

--- main.js
+++ main.js
@@ -58,22 +58,13 @@ function get_bio () {
 }
 
 const ig = new IgApiClient();
-// You must generate device id's before login.
-// Id's generated based on seed
-// So if you pass the same value as first argument - the same id's are generated every time
 ig.state.generateDevice(USERNAME);
 (async () => {
-  // Execute all requests prior to authorization in the real Android application
-  // Not required but recommended
   await ig.simulate.preLoginFlow();
   const loggedInUser = await ig.account.login(USERNAME, PASSWORD).catch(err => {
     console.log('Error logging in: ', err);
   });
   console.log(loggedInUser);
-  // The same as preLoginFlow()
-  // Optionally wrap it to process.nextTick so we dont need to wait ending of this bunch of requests
-  //process.nextTick(async () => await ig.simulate.postLoginFlow());
-  // Create UserFeed instance to get loggedInUser's posts
   const userFeed = ig.feed.user(loggedInUser.pk);
   console.log(userFeed);
 
@@ -84,14 +75,4 @@ while (true){
   await sleep(sleepTime);
     
 }
-//   await ig.media.like({
-//     // Like our first post from first page or first post from second page randomly
-//     mediaId: sample([myPostsFirstPage[0].id, myPostsSecondPage[0].id]),
-//     moduleInfo: {
-//       module_name: 'profile',
-//       user_id: loggedInUser.pk,
-//       username: loggedInUser.username,
-//     },
-//     d: sample([0, 1]),
-//   });
 })();
